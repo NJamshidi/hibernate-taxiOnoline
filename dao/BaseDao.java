73733b59564ab97bd3e106c90a7eeaa08f2037f5
unknown <narges40jam67@gmail.com>
2021-12-28 13:56:16 +0330

    add config and baseDao

--- dao/BaseDao.java
+++ dao/BaseDao.java
@@ -1,4 +1,8 @@
 package tamrintaxi.dao;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class BaseDao {
+    static SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 }
